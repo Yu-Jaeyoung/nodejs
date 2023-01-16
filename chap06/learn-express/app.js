1ca0054d51628e73688ae6f20533738486f37bfa
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-17 03:11:52 +0900

    Router Example

--- chap06/learn-express/app.js
+++ chap06/learn-express/app.js
@@ -5,41 +5,39 @@ import session from 'express-session';
 import dotenv from 'dotenv';
 import path from 'path';
 
-
-const __dirname = path.resolve();
 dotenv.config();
+
+import indexRouter from './routes';
+import userRouter from './routes/user';
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use('/',express.static(path.join(__dirname,'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: process.env.COOKIESECRET,
-    cookie:{
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
         httpOnly: true,
         secure: false,
     },
     name: 'session-cookie',
 }));
 
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
 app.use((req, res, next) => {
-    console.log("모든 요청에 다 실행됩니다.");
-    next();
-});
-app.get('/', (req, res, next) => {
-    console.log("GET / 요청에서만 실행됩니다.");
-    next();
-}, (req, res) => {
-    throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
+    res.status(404).send('Not Found');
 });
 
 app.use((err, req, res, next) => {
-    console.log(err);
+    console.error(err);
     res.status(500).send(err.message);
 });
 
