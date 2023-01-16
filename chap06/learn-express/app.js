7c6e5904f415acac2ee4e7d689b14629f0a25812
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-16 15:15:54 +0900

    modified app.js

--- chap06/learn-express/app.js
+++ chap06/learn-express/app.js
@@ -1,7 +1,31 @@
 import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import path from 'path';
 
+
+const __dirname = path.resolve();
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/',express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIESECRET,
+    cookie:{
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
 
 app.use((req, res, next) => {
     console.log("모든 요청에 다 실행됩니다.");
