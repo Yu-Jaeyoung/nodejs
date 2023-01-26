e14570ca8e08a571e2f26e5d6a82f02f99f38d14
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-26 18:26:00 +0900

    define model

---
+++ chap07/learn-sequelize/models/comment.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+
+class Comment extends Sequelize.Model {
+  static initiate(sequelize) {
+    Comment.init({
+      comment: {
+        type: Sequelize.STRING(100),
+        allowNULL: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNULL: true,
+        defaultValue: Sequelize.NOW,
+      },
+    }, {
+      sequelize,
+      timestamps: false,
+      modelName: "Comment",
+      tableName: "comments",
+      paranoid: false,
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci",
+    });
+  }
+
+  static associate(db) {
+    db.Comment.belongsTo(db.User, {foreignKey: "commenter", tartgetKey: "id"});
+  }
+};
+
+module.exports = Comment;
---
+++ chap07/learn-sequelize/models/user.js
@@ -0,0 +1,44 @@
+const Sequelize = require("sequelize");
+
+class User extends Sequelize.Model {
+  static initiate(sequelize) {
+    User.init({
+      name: {
+        type: Sequelize.STRING(20),
+        allowNULL: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNULL: false,
+      },
+      married: {
+        type: Sequelize.BOOLEAN,
+        allowNULL: false,
+      },
+      comment: {
+        type: Sequelize.TEXT,
+        allowNULL: true,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNULL: false,
+        defaultValue: Sequelize.NOW,
+      },
+    }, {
+      sequelize,
+      timestamps: false,
+      underscored: false,
+      modelName: "User",
+      tableName: "Users",
+      paranoid: false,
+      charset: "utf8",
+      collate: "utf8_general_ci",
+    });
+  }
+
+  static associate(db) {
+  }
+};
+
+module.exports = User;
