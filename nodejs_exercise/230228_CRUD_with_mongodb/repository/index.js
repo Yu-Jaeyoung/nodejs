1133eb702f31df9ebc0fc32b060005f50a24659f
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-11 15:41:35 +0900

    resolved problem / CRUD_with_mongodb

--- nodejs_exercise/230228_CRUD_with_mongodb/repository/index.js
+++ nodejs_exercise/230228_CRUD_with_mongodb/repository/index.js
@@ -1,5 +1,5 @@
 import connect from "./connect.js";
-
+import {ObjectId} from "mongodb";
 
 /*
 // Example form
@@ -11,32 +11,42 @@ const newUser = {
 };
 */
 
+// JS 공부 진행 후 이해해야될 부분
+// 왜 return 하는 과정에서도 await이 붙어야할까 ??
+
 // create
 export async function registerUser(newUser) {
   const connection = await connect();
-  return connection.insertOne(newUser);
+  return await connection.insertOne(newUser);
 }
 
-// read
+
+// get
+// get 부분에서는 toArray 처리를 하지 않고, await을 붙이면 경고가 발생하는데 왜그러는 걸까?
+// toArray() 유무에 따라 성공 여부가 갈리는 것을 확인할 수 있었음
+
 export async function findAll() {
   const connection = await connect();
-  return connection.find({});
+  return await connection.find({}).toArray();
 }
 
-export async function findById(email) {
+export async function findIdByEmailPassword(email, password) {
   const connection = await connect();
-  return connection.find({"email": email});
+  const foundUser = await connection.findOne({"email": email, "password": password});
+  return new ObjectId(foundUser["_id"]); // ObjectId를 씌우는 작업?
 }
-
 
 // update
-
-
-// delete
-export async function deleteUserByEmailAndPassword(email, password) {
+export async function updateById(_id, nickName) {
   const connection = await connect();
-  connection.deleteOne({"email": email, "password": password});
+  return await connection.updateOne({"_id": _id}, {$set: {"nickName": nickName}});
 }
 
+// delete
+
+export async function deleteById(_id) {
+  const connection = await connect();
+  return await connection.deleteOne({"_id": _id});
+}
 
 
--- nodejs_exercise/230228_CRUD_with_mongodb/routes/user.js
+++ nodejs_exercise/230228_CRUD_with_mongodb/routes/user.js
@@ -1,27 +1,24 @@
 import express from "express";
 import {
-  registerUser, findAll, findById,
+  registerUser, findAll, updateById, findIdByEmailPassword, deleteById,
 } from "../repository/index.js";
 
 const router = express.Router();
 
-// 해야할일
-// status(200) 반환으로 성공 여부 반환하기
-// 사용자 등록은 status(201)
 // npm install http-status-codes --save // status code 를 자동으로 불러와주는
-// get을 할때는 바디로 받는게 없음
-// res처리를 해줘야지
-
 
 router.route("/api/users")
+  // 등록된 대상 전체에 대해서 출력 진행
   .get(async (req, res) => {
     const users = await findAll();
     console.log(`${JSON.stringify(users)}`);
-    console.log(users);
     res.status(200).send(`${JSON.stringify(users)}`);
   })
-  // 이메일, 비밀번호, 유저이름, 닉네임 입력 시 등록 가능
+
+  // 이메일, 비밀번호, 유저이름, 닉네임 입력 시 등록 가능하게 설정
+  // 실패한 경우엔 콘솔에 만찍어서 보냄 / post 요청에 대해 res 보낼 필요는 없을 듯
   .post(async (req, res) => {
+    console.log(req.body);
     if (req.body["email"] && req.body["password"] && req.body["userName"] && req.body["nickName"]) {
       await registerUser(req.body);
       res.status(201).send(`${JSON.stringify(req.body)}`);
@@ -30,23 +27,49 @@ router.route("/api/users")
       console.log("Create Failed");
     }
   })
-  .delete((req, res) => {
-    deleteUserByEmailAndPassword(req.body["email"], req.body["password"]);
-  });
 
-router.route("/api/users/:userId")
   .patch(async (req, res) => {
+    // body로는 email, password, nickname을 받음
+    // body로 들어온 정보 중 닉네임 변경 진행 예정
+
+    // 콘솔에 들어온 body의 내용을 stringify해서 찍음
     console.log(`${JSON.stringify(req.body)}`);
-    console.log();
-    const findUser = await findById(req.params.userId);
-    if ((findUser.email === req.body.email) && (findUser.password === req.body.password)) {
-      console.log(req.params.userId);
-      const result = await updateById(req.params.userId, findUser.nickName);
-      res.status(200)
-        .send(result);
-    } else {
-      console.log("sorry, I can't update");
+
+    // email, password 이용해 id를 받아옴
+    const foundUserId = await findIdByEmailPassword(req.body.email, req.body.password);
+
+    // 찾은 id를 활용, 대상에 대해서 닉네임 patch 진행
+    // 문제점은 foundUserId가 존재하는 경우에만 updateById를 불러와서 실행하고 싶은데
+    // 그 과정을 (!foundUserId)를 활용하게 되면 작동이 안하는 상황이 발생
+    // 이 부분은 JS 문법을 공부 한후 수정하는 과정이 필요해보임
+    const result = await updateById(foundUserId, req.body.nickName);
+    res.status(200).send(result);
+
+    /*
+     else {
+      res.status(404).send("Patch Failed");
     }
+    */
+  })
+
+  .delete(async (req, res) => {
+    // body로 delete할 대상의 아이디와 비밀번호를 받음
+    // 콘솔에 들어온 body의 내용 출력
+    console.log(`${JSON.stringify(req.body)}`);
+
+    // email, password를 이용해 사용자 id를 받아옴
+    // 해당 대상이 존재한다면
+    const foundUserId = await findIdByEmailPassword(req.body.email, req.body.password);
+
+    // 찾은 id를 활용해서 대상 삭제 진행
+    // 이 부분도 역시 (!foundUserId)를 활용하면 정상적으로 작동하지 않음
+    const result = await deleteById(foundUserId);
+    res.status(200).send(result);
+    /*
+    else {
+      res.status(404).send("Delete Failed");
+    }
+    */
   });
 
 
